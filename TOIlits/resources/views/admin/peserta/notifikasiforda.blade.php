faf08e41e0a07c6a588a3df75242e6ba57a9ca35
unknown <yogacahya08@gmail.com>
2019-12-23 22:55:42 +0700

    Mengubah kontak menjadi perwakilan

--- TOIlits/resources/views/admin/peserta/notifikasiforda.blade.php
+++ TOIlits/resources/views/admin/peserta/notifikasiforda.blade.php
@@ -8,14 +8,19 @@
 @stop
 
 @section('content')
-    @foreach($notif as $n)
-        <div class="alert alert-success" role="alert">
-            <strong>{{ \Carbon\Carbon::parse($n->updated_at)->format('d-m-Y') }}</strong><br>
-            {{ $n->pengumuman}}
-        </div>
-    @endforeach
-
-    <p style="position:absolute;bottom:0"><b>Untuk informasi lebih lanjut hubungi : {{$forda->hp_ketua}} ({{$forda->nama_ketua}})</b></p>
+@if(count($notif)<1)
+<h5>Tidak ada notifikasi</h5>
+@else
+@foreach($notif as $n)
+<div class="alert alert-success" role="alert">
+    <strong>{{ \Carbon\Carbon::parse($n->updated_at)->format('d-m-Y') }}</strong><br>
+    {{ $n->pengumuman}}
+</div>
+@endforeach
+@endif
+@if($forda->hp_perwakilan!="-")
+<p style="position:absolute;bottom:0"><b>Untuk informasi lebih lanjut hubungi <u>{{$forda->nama_perwakilan}}</u> : {{$forda->hp_perwakilan}}@if($forda->id_line_perwakilan!="-") / {{$forda->id_line_perwakilan}}(line)@endif </b></p>
+@endif
 @stop
 
 @section('css')
